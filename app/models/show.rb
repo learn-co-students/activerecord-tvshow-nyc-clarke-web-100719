d4186cf415f1ae8fe2d42f8d8ac6846470236ba2
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-17 10:41:08 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
     def self.highest_rating
-        self.maximum "rating"
+        self.maximum :rating
     end
 
     def self.most_popular_show
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
     end
 
     def self.lowest_rating
-        self.minimum "rating"
+        self.minimum :rating
     end
 
     def self.least_popular_show
@@ -16,14 +16,14 @@ class Show < ActiveRecord::Base
     end
 
     def self.ratings_sum
-        self.sum "rating"
+        self.sum :rating
     end
 
     def self.popular_shows
-        self.where "rating > 5"
+        self.where "rating > ?", 5
     end
 
     def self.shows_by_alphabetical_order
-        self.order "name" 
+        self.order :name 
     end
 end
